78b54ac9b3707c4671143b914fd263f31c6bc172
fangmj <fangmj@jizegame.com>
2018-12-25 23:25:08 +0800

    add comments on initialize function

--- lib/fileminer.rb
+++ lib/fileminer.rb
@@ -3,15 +3,21 @@ require 'json'
 
 class Miner
 
-  def initialize registry_path, paths, output
-    @registry_path = registry_path
-    @paths = paths
-    @output = output
+  # Create a new file miner instance
+  #
+  # @param [Hash] options
+  # @option options [String] :registry_path
+  # @option options [Array] :paths
+  # @option options [OutputPlugin] :output
+  def initialize(options = {})
+    @registry_path = options[:registry_path]
+    @paths = options[:paths]
+    @output = options[:output]
     @registry = []
     if File.exist? registry_path
       File.open(registry_path) { |io| @registry = JSON.parse(io.read, {symbolize_names: true}) }
     end
-    @files = @registry.map { |e| [e['path'], e] }
+    @files = (@registry.map { |e| [e['path'], e] }).to_h
     # TODO
   end
 
