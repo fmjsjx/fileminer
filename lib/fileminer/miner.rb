e255b23d2d05fc67c58ae533fb2cb4a7ce095b71
fangmj <fangmj@jizegame.com>
2018-12-27 23:10:31 +0800

    change tree

---
+++ lib/fileminer/miner.rb
@@ -0,0 +1,90 @@
+require 'set'
+require 'json'
+
+
+class Dir
+
+  class << self
+
+    # Creates the directory with the path given, including anynecessary but nonexistent parent directories.
+    #
+    # @param [String] path
+    def mkdirs(path)
+      parent = File.dirname path
+      mkdirs parent unless Dir.exist? parent
+      Dir.mkdir path
+    end
+
+  end
+
+end
+
+
+class Miner
+
+  DEFAULTS = {
+    registry_path: '/var/lib/fileminer/registry',
+    eof_seconds: 86400,
+    batch_lines: 50,
+  }
+
+  attr_reader :registry_path, :paths, :eof_seconds, :output, :file_list
+
+  # Create a new file miner instance
+  #
+  # @param [Hash] options
+  # @option options [String] :registry_path (/var/lib/fileminer/registry)
+  # @option options [Array] :paths
+  # @option options [Integer] :eof_seconds (86400)
+  # @option options [OutputPlugin] :output
+  # @option options [Integer] :batch_lines (50)
+  def initialize(options = {})
+    # fix options by DEFAULTS
+    DEFAULTS.each { |k, v| options[k] = v unless options.key? k }
+    @registry_path = options[:registry_path]
+    @paths = options[:paths]
+    @eof_seconds = options[:eof_seconds]
+    @output = options[:output]
+    @batch_lines = options[:batch_lines]
+    @file_list = []
+    if File.exist? @registry_path
+      File.open(@registry_path) { |io| @file_list = JSON.parse(io.read, {symbolize_names: true}) }
+    else
+      parent_dir = File.dirname @registry_path
+      Dir.mkdirs parent_dir unless Dir.exist? parent_dir
+    end
+  end
+
+  # Save registry
+  def save_registry
+    File.open(@registry_path, 'w') { |io| io.write @file_list.to_json }
+  end
+
+  # Refresh
+  def refresh_file_list
+    file_paths = Set.new
+    @paths.each do |path|
+      file_paths.merge Dir[path].select { |path| File.file? path }
+    end
+    @file_list.each do |record|
+      path = record[:path]
+      unless record[:eof]
+        if file_paths.delete? path
+          # check if EOF
+          if record[:pos] == File.size(path) && Time.now - File.mtime(path) > @eof_seconds
+            record[:eof] = true
+          end
+        else
+          # missing file, set :eof to true
+          record[:eof] = true
+        end
+      end
+    end
+    file_paths.each do |path|
+      record = {path: path, pos: 0, eof: false}
+      @file_list << record
+    end
+    @file_list_refresh_time = Time.now
+  end
+
+end
